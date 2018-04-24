3dfde7d6e3961caa8c9dd0766ae5877f42358f6f
Jin Tak <jkt0904@p2es.com>
2018-04-24 17:46:15 -0600

    project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, stagger, style, transition, animate, keyframes, query } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  animations: [
+
+    trigger('projectCards', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+
+        // query(':enter', stagger('100ms', [
+        //   animate('1s ease-in', keyframes([
+        //     style({ opacity: 0, transform: 'rotateY(0)', offset: 0 }),
+        //     style({ opacity: .5, transform: 'rotateY(180deg)', offset: .3 }),
+        //     style({ opacity: 1, transform: 'rotateY(360deg)', offset: 1 }),
+        //   ]))]), { optional: true })
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+        ]))]), { optional: true })
+      ])
+    ])
+  ]
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  projects: string[] = [
+    'Easy Mass',
+    'Holistic Blog',
+    'Korean American School',
+    'Denver Nuggets',
+    'Space Racer',
+    'hello'
+  ];
 
 }
