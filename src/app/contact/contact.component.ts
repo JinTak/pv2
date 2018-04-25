7817617115d8719874cc517e336feb422c64288a
Jin Tak <jintak@Jins-MacBook-Pro.local>
2018-04-25 13:58:42 -0600

    contact form. added styles and formcarry

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, stagger, style, transition, animate, keyframes, query } from '@angular/animations';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
 }
