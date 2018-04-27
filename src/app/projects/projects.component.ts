51e279d5b302219ff57b698aef656ade896401aa
Jin Tak <jkt0904@p2es.com>
2018-04-27 15:39:51 -0600

    still projectssss

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,11 +11,11 @@ import { trigger, stagger, style, transition, animate, keyframes, query } from '
       transition('* => *', [
         query(':enter', style({ opacity: 0 }), { optional: true }),
 
-        query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
-            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
-            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+        query(':enter', stagger('400ms', [
+          animate('1.5s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateX(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateX(35px)', offset: .3 }),
+            style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
         ]))]), { optional: true })
       ])
     ])
